abab1b1706b21e6c473fa28ccb7a08ab841dee97
Pragnesh Shrimal <iampragnesh05@gmail.com>
2021-06-24 12:17:18 +0530

    My Blogging website ready for deployment.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
-
+const port = process.env.PORT || 3000;
 const app = express();
 app.set('view engine', 'ejs');
 
@@ -81,6 +81,6 @@ app.get("/posts/:postName", (req,res) => {
 
 
 
-app.listen(3000, () =>{
+app.listen(port, () =>{
 console.log("Server started on port 3000")
 })
